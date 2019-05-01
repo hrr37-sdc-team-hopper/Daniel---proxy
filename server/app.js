d3ee4f3b8a0b5789ff4e303ba8ef991bc42f220a
Danielxburns <danielburnsart@gmail.com>
2019-05-01 13:49:39 -0500

    replace proxy middleware and use local service

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const proxy = require('http-proxy-middleware');
 const morgan = require('morgan');
 const cors = require('cors');
 
@@ -8,5 +9,11 @@ app.use(morgan('dev'));
 
 const staticPath = `${__dirname}/../public`;
 app.use('/books/:id', express.static(staticPath));
+
+// reviews-service
+app.use(
+  '/books/:id/reviews',
+  proxy({ target: 'http://localhost:3003', changeOrigin: true }),
+);
 
 module.exports = app;
